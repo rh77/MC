3389c6a12190848e5eb44fd9fbdd25869507565f
rh77 <rhawaiian@gmail.com>
2019-01-06 20:42:59 +0300

    added calendar to slider-view

--- blocks/slider-view/slider-view.js
+++ blocks/slider-view/slider-view.js
@@ -1,15 +1,58 @@
 import { Component } from '../component';
 import template from './slider-view.pug';
 import _ from './slider-view.scss';
+import { Calendar } from '../calendar/calendar';
 
 export class SliderView extends Component {
 
   constructor(data) {
     super(data);
+
+    this.TOTAL_SIGNS = 12;
+    this.TOTAL_COLUMNS = 14;
+    this.TOTAL_PREDICTIONS = 9;
   }
 
   render() {
 
     this._el.innerHTML = template();
+
+    let prediction = this._getPrediction();
+    this.calendar = new Calendar({
+      el: this._el.querySelector(".js-calendar"),
+      options: {
+        width: prediction.columns.length,
+        height: this.TOTAL_SIGNS,
+        content: prediction
+      } 
+    });
+
+    this.calendar.render();
+  }
+
+  _getPrediction() {
+
+    let dateNow = new Date();
+    let prediction = {
+      columns: [],
+      type: "day",
+      dates: []
+    };
+
+    for (let i = 0; i < this.TOTAL_COLUMNS; i++)
+    {
+      let predictions = [];
+      for (let j = 0; j < this.TOTAL_SIGNS; j++)
+      {
+        predictions.push((j + i) % this.TOTAL_PREDICTIONS + 1);
+      }
+
+      prediction.columns.push(predictions);
+
+      let date = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + i);
+      prediction.dates.push(date);
+    }
+
+    return prediction;
   }
 }
