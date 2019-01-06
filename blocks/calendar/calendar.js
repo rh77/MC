4dcc6208b6bbabece663406b220dc11839c42d5c
rh77 <rhawaiian@gmail.com>
2019-01-06 14:23:23 +0300

    fix calendar view and preview

--- blocks/calendar/calendar.js
+++ blocks/calendar/calendar.js
@@ -1,5 +1,6 @@
 import { Component } from '../component';
 import { CalendarDateCell } from './__date-cell/calendar__date-cell';
+import { PredictionSign } from '../prediction-sign/prediction-sign';
 import template from './calendar.pug';
 import _ from './calendar.scss';
 
@@ -18,14 +19,40 @@ export class Calendar extends Component {
 
     this._el.innerHTML = template(options);
 
-    this.dateCell = new CalendarDateCell({
-      el: document.querySelector('.js-date-cell-1')
-    });
+    let prediction = this._options.content;
 
-    this.dateCell.render({
-      type: "day",
-      value: new Date()
-    });
+    let columns = this._el.querySelectorAll('.js-column');
+    for (let i = 0; i < columns.length; i++)
+    {
+      let column = columns[i];
+      let dateCell = new CalendarDateCell({
+        el: column.querySelector('.js-date-cell')
+      });
+
+      dateCell.render({
+        type: prediction.type,
+        value: prediction.dates[i]
+      });
+
+      let columnData = prediction.columns[i];
+      let predictionElements = column.querySelectorAll('.js-prediction-cell');
+
+      for (let j = 0; j < predictionElements.length; j++)
+      {
+        let predictionCell = new PredictionSign({
+          el: predictionElements[j]
+        });
+
+        predictionCell.render({
+          prediction: columnData[j]
+        });
+      }
+    }
   }
 
+  moveRight() {
+
+    let firstColumn = this._el.querySelector('.js-column');
+    firstColumn.parentNode.appendChild(firstColumn);
+  }
 }
