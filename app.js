1c46de30b29d76b7b3cefde84831728ffc486c7e
rh77 <rhawaiian@gmail.com>
2019-01-08 20:14:23 +0300

    added year preview

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import { Calendar } from './blocks/calendar/calendar';
 import { PredictionSign } from './blocks/common/prediction-sign/prediction-sign';
 import { SliderView } from './blocks/slider-view/slider-view';
 import { YearSignList } from './blocks/year-sign-list/year-sign-list';
+import { SliderViewDayStrategy } from './blocks/slider-view/slider-view-day-strategy';
+import { SliderViewYearStrategy } from './blocks/slider-view/slider-view-year-strategy';
 
 window.Calendar = Calendar;
 window.PredictionSign = PredictionSign;
 window.SliderView = SliderView;
-window.YearSignList = YearSignList
+window.YearSignList = YearSignList;
+window.SliderViewDayStrategy = SliderViewDayStrategy;
+window.SliderViewYearStrategy = SliderViewYearStrategy;
